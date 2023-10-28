cd6daf60a262cd8f6e2bee6acdccbd6f4fe8ddf8
Alvin Chong <alvinchong715@gmail.com>
2023-10-28 13:44:47 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+let myName=prompt("What is your name?")
+let click=document.querySelector("clickButton")
+click.addEventListener("click",myclick())
+function myClick(){
+    alert('Welcome ${myName}')
+    const favweather = prompt("What is your favourite weather?")
+    let myImage = document.querySelector(".weatherImage")
+    if favweather.toLowerCase()==="Sunny"{
+        myImage.scr="/image/sunny.jpg"
+
+    }else if {favweather.toLowerCase()==="Rainy"{
+        myImage.scr="/image/rainy.jpg"}
+}
+
+console.log("Hello, World")
+
+document.querySelector("# my button")
+
+
+alert("Hello"+myName)
